31e028f07fed0df8d2ec0eee804a3f699902cbc8
Justin Kim <justinkim943@gmail.com>
2019-04-28 01:00:58 -0700

    got detailed page up and running

--- src/router.js
+++ src/router.js
@@ -28,36 +28,68 @@ router.get('/', (req,res) => {
 
 router.get('/projects', async (req,res) => {
     try {
-        // const apiResponse = await axios.get(`${baseURL}/v1/projects`, {
-        //     params: {
-        //         api_key: process.env.HACKADAY_API_KEY
-        //     }
-        // });
+        const apiResponse = await axios.get(`${baseURL}/v1/projects`, {
+            params: {
+                api_key: process.env.HACKADAY_API_KEY
+            }
+        });
 
         // implementing faker
-        let apiResponse = { data: { projects: [] } };
-        for ( let i=0; i<50; i++) {
-            const newData = {
-                name: faker.name.title(),
-                image_url: faker.image.image(),
-                screen_name: faker.name.firstName(),
-                avatar: faker.image.avatar(),
-                summary: faker.lorem.sentence()
-            }
-            apiResponse.data.projects.push(newData);
-        }
+        // let apiResponse = { data: { projects: [] } };
+        // for ( let i=0; i<50; i++) {
+        //     const newData = {
+        //         name: faker.name.title(),
+        //         image_url: faker.image.image(),
+        //         screen_name: faker.name.firstName(),
+        //         avatar: faker.image.avatar(),
+        //         summary: faker.lorem.sentence()
+        //     }
+        //     apiResponse.data.projects.push(newData);
+        // }
 
         let projects = apiResponse.data.projects;
-        // for ( let i=0; i<projects.length; i++) {
-        //     const userResponse = await axios.get(`${baseURL}/v1/users/${projects[i].owner_id}`, {
-        //         params: {
-        //             api_key: process.env.HACKADAY_API_KEY
-        //         }
-        //     });
-        //     projects[i]["screen_name"] = userResponse.data.screen_name;
-        //     projects[i]["avatar"] = userResponse.data.image_url;
-        // }
+        for ( let i=0; i<projects.length; i++) {
+            const userResponse = await axios.get(`${baseURL}/v1/users/${projects[i].owner_id}`, {
+                params: {
+                    api_key: process.env.HACKADAY_API_KEY
+                }
+            });
+            projects[i]["screen_name"] = userResponse.data.screen_name;
+            projects[i]["avatar"] = userResponse.data.image_url;
+        }
         return res.render('index', {projects} );
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+});
+
+router.get('/projects/:id', async (req,res) => {
+    try {
+        const projectId = req.params.id;
+        const apiResponse = await axios.get(`${baseURL}/v1/projects/${projectId}`, {
+            params: {
+                api_key: process.env.HACKADAY_API_KEY
+            }
+        });
+
+        let project = apiResponse.data;
+
+        const user = await axios.get(`${baseURL}/v1/users/${project.owner_id}`, {
+            params: {
+                api_key: process.env.HACKADAY_API_KEY
+            }
+        });
+
+        const tag = project.tags[0];
+        let relatedProjects = await axios.get(`${baseURL}/v1/search/projects`, {
+            params: {
+                api_key: process.env.HACKADAY_API_KEY,
+                search_term:tag
+            }
+        });
+        relatedProjects = relatedProjects.data.projects.slice(0,9);
+        return res.render('detail', {project, user: user.data, relatedProjects})
+
     } catch (e) {
         res.status(500).send({error: e.message});
     }
